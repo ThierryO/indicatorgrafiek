b93053305ca59d1853023328f446d4de38abc9cf
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2022-02-18 16:36:43 +0100

    use random base plot

--- app.R
+++ app.R
@@ -10,6 +10,11 @@ soort_d <- read_vc("soort_d")
 soort_e <- read_vc("soort_e")
 deze_ref <- read_vc("deze_ref")
 
+base_plot <- list(
+  a = ggplot(soort_a), b = ggplot(soort_b), c = ggplot(soort_c),
+  d = ggplot(soort_d), e = ggplot(soort_e)
+)
+
 # Define UI for application that draws a histogram
 ui <- fluidPage(
   sidebarLayout(
@@ -27,14 +32,17 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+  data <- reactiveValues(
+    base = sample(base_plot, 1)[[1]]
+  )
   output$graph_a <- renderPlot({
-    ggplot(data = soort_c, aes(x = naar, y = schatting)) +
-      geom_line() +
+    data$base +
+      geom_line(aes(x = naar, y = schatting)) +
       ggtitle("A") + theme(plot.title = element_text(hjust = 0))
   })
   output$graph_b <- renderPlot({
-    ggplot(data = soort_c, aes(x = naar, y = schatting)) +
-      geom_point() +
+    data$base +
+      geom_point(aes(x = naar, y = schatting)) +
       ggtitle("B") + theme(plot.title = element_text(hjust = 0))
   })
 }
