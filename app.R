c3e92a4c38394023e71608de3ef8b0f845424eed
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2022-02-18 20:56:01 +0100

    handle the users choice

--- app.R
+++ app.R
@@ -31,6 +31,10 @@ rev_labels <- function(x) {
 index_labels <- function(x) {
   sprintf("%.0f", 100 * exp(x))
 }
+base_plot <- list(
+  a = ggplot(soort_a), b = ggplot(soort_b), c = ggplot(soort_c),
+  d = ggplot(soort_d), e = ggplot(soort_e)
+)
 y_scale <- list(
   index = scale_y_continuous(
     "index (2007 = 100)", breaks = index_breaks, labels = index_labels
@@ -49,15 +53,15 @@ href <- list(
   tekst = list(
     geom_hline(yintercept = c(0, 1, -1) * log(drempel), linetype = c(1, 2, 2)),
     annotate(
-      "text", x = Inf, y = 0, vjust = -0.1, hjust = 1, label = "referentie",
+      "text", x = Inf, y = 0, vjust = -0.3, hjust = 1, label = "referentie",
       colour = inbo_steun_donkerroos
     ),
     annotate(
-      "text", x = Inf, y = log(drempel), vjust = -0.1, hjust = 1,
+      "text", x = Inf, y = log(drempel), vjust = -0.3, hjust = 1,
       label = "ondergrens", colour = inbo_steun_donkerroos
     ),
     annotate(
-      "text", x = Inf, y = -log(drempel), vjust = -0.1, hjust = 1,
+      "text", x = Inf, y = -log(drempel), vjust = -0.3, hjust = 1,
       label = "bovengrens", colour = inbo_steun_donkerroos
     )
   ),
@@ -116,14 +120,76 @@ uncertainty <- list(
   )
 )
 
-# Define UI for application that draws a histogram
+define_user_input <- function() {
+  expand_grid(
+    href_a = factor(names(href)), href_b = factor(names(href)),
+    pref_a = factor(names(pref)), pref_b = factor(names(pref)),
+    trend_a = factor(names(trend)), trend_b = factor(names(trend)),
+    uncertainty_a = factor(names(uncertainty)),
+    uncertainty_b = factor(names(uncertainty)),
+    y_scale_a = factor(names(y_scale)), y_scale_b = factor(names(y_scale))
+  ) %>%
+    filter(
+      (.data$href_a != .data$href_b) | (.data$pref_a != .data$pref_b) |
+        (.data$trend_a != .data$trend_b) | (.data$y_scale_a != .data$y_scale_b) |
+        (.data$uncertainty_a != .data$uncertainty_b)
+    ) %>%
+    mutate(id = row_number()) -> full_grid
+  full_grid %>%
+    mutate(a = 0L, b = 0L)
+}
+
+get_information <- function(x, variable) {
+  x %>%
+    select(
+      var_a = .data[[paste0(variable, "_a")]], .data$a,
+      var_b = .data[[paste0(variable, "_b")]], .data$b
+    ) %>%
+    filter(.data$var_a != .data$var_b) %>%
+    pivot_longer(
+      c(-"a", -"b"), names_to = "test", values_ptypes = character()
+    ) %>%
+    mutate(
+      win = ifelse(str_detect(.data$test, "_a"), .data$a, .data$b),
+    ) %>%
+    group_by(.data$value) %>%
+    summarise(
+      wins = sum(.data$win), tests = sum(.data$a + .data$b), .groups = "drop"
+    ) %>%
+    transmute(
+      element = variable, .data$value,
+      prop = (.data$wins + 1) / sum(.data$wins + 1),
+      shannon = sum(-.data$prop * log(.data$prop)),
+      prop = .data$prop / (.data$tests + 1)
+    )
+}
+
+sample_combination <- function(x) {
+  to_do <- c("href", "pref", "trend", "uncertainty", "y_scale")
+  while (length(to_do)) {
+    proportions <- map_dfr(to_do, get_information, x = x)
+    proportions %>%
+      distinct(.data$element, .data$shannon) %>%
+      slice_max(.data$shannon, n = 1) %>%
+      semi_join(x = proportions, by = "element") %>%
+      slice_sample(n = 1, weight_by = .data$prop) -> selected
+    x %>%
+      filter(
+        (.data[[paste0(selected$element, "_a")]] == selected$value) |
+        (.data[[paste0(selected$element, "_b")]] == selected$value)
+      ) -> x
+    to_do <- to_do[to_do != selected$element]
+  }
+  x %>%
+    slice_sample(n = 1)
+}
+
 ui <- fluidPage(
   sidebarLayout(
     sidebarPanel = sidebarPanel(
       width = 1,
-      radioButtons(
-        "voorkeur", "voorkeur", choices = c("", "A", "B"), selected = ""
-      )
+      actionButton("kies_a", label = "kies A"),
+      actionButton("kies_b", label = "kies B")
     ),
     mainPanel = mainPanel(
       width = 11, plotOutput("graph_a"), plotOutput("graph_b")
@@ -131,24 +197,36 @@ ui <- fluidPage(
   )
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
   data <- reactiveValues(
     base = sample(names(base_plot), 1),
-    href = sample(names(href), 2, replace = FALSE),
-    pref = sample(names(pref), 2, replace = TRUE),
-    trend = sample(names(trend), 2, replace = TRUE),
-    uncertainty = sample(names(uncertainty), 2, replace = TRUE),
-    y_scale = sample(names(y_scale), 2, replace = TRUE)
+    user_input = define_user_input()
   )
+
+  combination <- reactive({
+    if (nrow(data$user_input) == 0) {
+      return(NULL)
+    }
+    sample_combination(data$user_input) %>%
+      select(-.data$a, -.data$b) -> tmp
+    tmp %>%
+      pivot_longer(-.data$id, values_ptypes = character()) %>%
+      extract(.data$name, c("element", "level"), "(.*)_(.*)") %>%
+      pivot_wider(names_from = .data$level, values_from = .data$value)
+  })
+
   output$graph_a <- renderPlot({
+    df <- combination()
+    if (is.null(df)) {
+      return(NULL)
+    }
     p <- base_plot[[data$base]] +
       ggtitle("A") +
       theme(
         axis.title.x = element_blank(), plot.title = element_text(hjust = 0)
       ) +
-      y_scale[[data$y_scale[1]]]
-    h <- href[[data$href[1]]]
+      y_scale[[df$a[df$element == "y_scale"]]]
+    h <- href[[df$a[df$element == "href"]]]
     if (!is.null(h)) {
       if (inherits(h, "list")) {
         for (z in h) {
@@ -158,7 +236,7 @@ server <- function(input, output) {
         p <- p + h
       }
     }
-    h <- href[[data$pref[1]]]
+    h <- href[[df$a[df$element == "pref"]]]
     if (!is.null(h)) {
       if (inherits(h, "list")) {
         for (z in h) {
@@ -168,7 +246,7 @@ server <- function(input, output) {
         p <- p + h
       }
     }
-    h <- uncertainty[[data$uncertainty[1]]]
+    h <- uncertainty[[df$a[df$element == "uncertainty"]]]
     if (inherits(h, "list")) {
       for (z in h) {
         p <- p + z
@@ -176,7 +254,7 @@ server <- function(input, output) {
     } else {
       p <- p + h
     }
-    h <- trend[[data$trend[1]]]
+    h <- trend[[df$a[df$element == "trend"]]]
     if (inherits(h, "list")) {
       for (z in h) {
         p <- p + z
@@ -186,14 +264,19 @@ server <- function(input, output) {
     }
     p
   })
+
   output$graph_b <- renderPlot({
+    df <- combination()
+    if (is.null(df)) {
+      return(NULL)
+    }
     p <- base_plot[[data$base]] +
       ggtitle("B") +
       theme(
         axis.title.x = element_blank(), plot.title = element_text(hjust = 0)
       ) +
-      y_scale[[data$y_scale[2]]]
-    h <- href[[data$href[2]]]
+      y_scale[[df$b[df$element == "y_scale"]]]
+    h <- href[[df$b[df$element == "href"]]]
     if (!is.null(h)) {
       if (inherits(h, "list")) {
         for (z in h) {
@@ -203,7 +286,7 @@ server <- function(input, output) {
         p <- p + h
       }
     }
-    h <- href[[data$pref[2]]]
+    h <- href[[df$b[df$element == "pref"]]]
     if (!is.null(h)) {
       if (inherits(h, "list")) {
         for (z in h) {
@@ -213,7 +296,7 @@ server <- function(input, output) {
         p <- p + h
       }
     }
-    h <- uncertainty[[data$uncertainty[2]]]
+    h <- uncertainty[[df$b[df$element == "uncertainty"]]]
     if (inherits(h, "list")) {
       for (z in h) {
         p <- p + z
@@ -221,7 +304,7 @@ server <- function(input, output) {
     } else {
       p <- p + h
     }
-    h <- trend[[data$trend[2]]]
+    h <- trend[[df$b[df$element == "trend"]]]
     if (inherits(h, "list")) {
       for (z in h) {
         p <- p + z
@@ -231,7 +314,16 @@ server <- function(input, output) {
     }
     p
   })
+
+  observeEvent(input$kies_a, {
+    data$user_input[data$user_input$id == combination()$id, "a"] <-
+      data$user_input[data$user_input$id == combination()$id, "a"] + 1
+  })
+
+  observeEvent(input$kies_b, {
+    data$user_input[data$user_input$id == combination()$id, "b"] <-
+      data$user_input[data$user_input$id == combination()$id, "b"] + 1
+  })
 }
 
-# Run the application
 shinyApp(ui = ui, server = server)
